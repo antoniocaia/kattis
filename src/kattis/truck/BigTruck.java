5deb31f2877c70feaaba38e09b27b15d5c9d2282
Antonio Caia <antonio.caia96@gmail.com>
2020-05-11 18:27:11 +0200

    Can't pass test case 25/46

--- src/kattis/truck/BigTruck.java
+++ src/kattis/truck/BigTruck.java
@@ -11,27 +11,28 @@ public class BigTruck {
 
 	public static void main(String[] args) {
 
-		int nodeQt = Integer.parseInt(scanner.nextLine());
-		List<Node> nodeList = Arrays.stream(scanner.nextLine().split(" "))
-								.map(x -> new Node(Integer.parseInt(x)))
-									.collect(Collectors.toList());
-		
+		//int nodeQt = Integer.parseInt(scanner.nextLine()); 
+		scanner.nextLine();
+		List<Node> nodeList = Arrays.stream(scanner.nextLine().split(" ")).map(x -> new Node(Integer.parseInt(x)))
+				.collect(Collectors.toList());
+
 		int linkQt = Integer.parseInt(scanner.nextLine());
-		
-		for(int i = 0; i < linkQt; i++) {
-			List<Integer> input = Arrays.stream(scanner.nextLine().split(" "))
-					.map(x -> Integer.parseInt(x))
-						.collect(Collectors.toList());
-			
-			
+
+		for (int i = 0; i < linkQt; i++) {
+			List<Integer> input = Arrays.stream(scanner.nextLine().split(" ")).map(x -> Integer.parseInt(x))
+					.collect(Collectors.toList());
+
 			nodeList.get(input.get(0) - 1).addNewLink(input.get(1) - 1, input.get(2));
 			nodeList.get(input.get(1) - 1).addNewLink(input.get(0) - 1, input.get(2));
 		}
-		
+
 		LinkedList<Integer> pathList = new LinkedList<>();
 		pathList.add(0);
 		int[] shortest = nodeList.get(0).shortestPath(nodeList, pathList, 0, nodeList.get(0).getItems());
-		System.out.println(shortest[0] + " " + shortest[1]);
+		if (shortest[0] == Integer.MAX_VALUE)
+			System.out.println("impossible");
+		else
+			System.out.println(shortest[0] + " " + shortest[1]);
 	}
 
 }
