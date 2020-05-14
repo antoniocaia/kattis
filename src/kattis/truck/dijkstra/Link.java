212eedda3f7a568418ba6d66d229bd996e27f4e5
Antonio Caia <antonio.caia96@gmail.com>
2020-05-14 13:51:48 +0200

    Solved with dijkstra alghoritm, still failing testcase 46/46 due to time limit

---
+++ src/kattis/truck/dijkstra/Link.java
@@ -0,0 +1,11 @@
+package kattis.truck.dijkstra;
+
+public class Link {
+	public int node;
+	public int distance;
+	
+	public Link(int node, int distance) {
+		this.node = node;
+		this.distance = distance;
+	}
+}
